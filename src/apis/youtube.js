321160032909a69c0ad09e4fca5e609f3c66dd95
zura0z <zuramazmishvili@gmail.com>
2020-10-03 20:50:25 +0400

    Initial commit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCDsaH0IhGdVOiCFsxU2mc2dOTrWxTt-hc";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 25,
-    key: KEY,
+    key: process.env.REACT_APP_KEY,
   },
 });
