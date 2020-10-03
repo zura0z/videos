976d8f63d42f4706764af91f74d19b6f35706955
zura0z <zuramazmishvili@gmail.com>
2020-10-03 14:51:15 +0400

    Updates

---
+++ precache-manifest.9349c77c763e6601037b37ab4c8f0e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ff74b847ef441bfe1147fbc83a270d",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "2c7d77beb4b4837e81fd",
+    "url": "/videos/static/css/main.4965ec2a.chunk.css"
+  },
+  {
+    "revision": "3ca71b351f4a2fbf27b5",
+    "url": "/videos/static/js/2.de1ca534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videos/static/js/2.de1ca534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7d77beb4b4837e81fd",
+    "url": "/videos/static/js/main.ecd00444.chunk.js"
+  },
+  {
+    "revision": "651b395ae9aa625b966c",
+    "url": "/videos/static/js/runtime-main.1a1d86e7.js"
+  }
+]);
